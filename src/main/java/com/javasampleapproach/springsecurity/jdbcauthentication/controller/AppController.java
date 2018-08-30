a7ba98543dcc04a19526524e472b5a11e9243593
Dell <marcin.grupawat@gmail.com>
2018-08-30 10:31:57 +0200

    Initial commit

---
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/controller/AppController.java
@@ -0,0 +1,11 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+
+
+}
